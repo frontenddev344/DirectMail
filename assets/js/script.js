3f5ea3e34937eed3e11fd89f636c195c6e846c17
frontenddev344 <141622370+frontenddev344@users.noreply.github.com>
2024-01-24 11:32:29 +0530

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,25 +72,15 @@ window.addEventListener("click", () => {
 })
 
 // sticky js
-
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-
-// Get the header
-// var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-// var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
+window.onscroll = function () {
+   var header = document.querySelector("header");
+   if (window.pageYOffset > 0) {
+     header.classList.add("sticky");
+   } else {
+     header.classList.remove("sticky");
+   }
+ };
+ 
 
 
 
